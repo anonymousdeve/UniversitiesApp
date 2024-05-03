8371790cbf168a231d358d1fac84b1c0d09333f7
ali <alihrhera1@gmail.com>
2024-05-03 23:36:53 +0300

    add Universities list module

--- universities/src/main/java/ali/hrhera/module/universities/ui/UniversitiesListFragment.kt
+++ universities/src/main/java/ali/hrhera/module/universities/ui/UniversitiesListFragment.kt
@@ -1,17 +1,10 @@
 package ali.hrhera.module.universities.ui
 
-import ali.hrhera.module.base.di.AppComponent
-import ali.hrhera.module.base.di.DaggerAppComponent
-import ali.hrhera.module.base.di.ViewModelFactory
-import ali.hrhera.module.base.di.ViewModelModule
 import ali.hrhera.module.base.ui.fragment.BaseFragment
 import ali.hrhera.module.universities.databinding.FragmentUniversitiesListBinding
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import javax.inject.Inject
 
 class UniversitiesListFragment : BaseFragment<FragmentUniversitiesListBinding>() {
     override fun intBinding(inflater: LayoutInflater, container: ViewGroup?) =
